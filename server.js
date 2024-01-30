37ffac667b4206bfd439b1dd4c8a23c50bc60d9b
Sabina777 <sabina.prajapati777@gmail.com>
2024-01-30 18:29:32 +0000

    Implemented basic socket connection handling with user connection and disconnection logs

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
 
 http.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-  });
+  console.log('Server running at http://localhost:3000');
+});
