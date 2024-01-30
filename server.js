e6bf37027482812363aed24a4e22ef214d3b8f52
Sabina777 <sabina.prajapati777@gmail.com>
2024-01-30 18:36:47 +0000

    io fun ction updated to  broadcast received messages to all connected clients

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
